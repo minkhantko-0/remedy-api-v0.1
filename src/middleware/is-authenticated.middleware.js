25503bde21bae7d76684b8d7c41240b30316f402
minkhantkoo <minkhant5771@gmail.com>
2022-12-01 02:25:19 +0630

    fixed error format in middleware

--- src/middleware/is-authenticated.middleware.js
+++ src/middleware/is-authenticated.middleware.js
@@ -1,9 +1,9 @@
 function isAuthenticated(req, res, next) {
-    if (req.token && req.admin) {
-        return next();
-    }
+  if (req.token && req.admin) {
+    return next();
+  }
 
-    return res.status(401).send('auth-error/unauthorized');
+  return res.status(401).send({ error: "auth-error/unauthorized" });
 }
 
 module.exports = isAuthenticated;
